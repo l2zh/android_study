b36a2360fda0ff6bc1438776575278271ef05458
l2zh <dlwnsgml0109@naver.com>
2023-01-22 12:37:19 +0900

    회원가입 페이지로 넘어가기

--- app/src/main/java/com/example/android_study/SignInActivity.kt
+++ app/src/main/java/com/example/android_study/SignInActivity.kt
@@ -1,11 +1,27 @@
 package com.example.android_study
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SignInActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+
+        val signup = findViewById<Button>(R.id.btn_signin_singup)
+
+        fun movetosignup(){
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        signup.setOnClickListener{
+            movetosignup()
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/android_study/SignupActivity.kt
+++ app/src/main/java/com/example/android_study/SignupActivity.kt
@@ -2,14 +2,14 @@ package com.example.android_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
 
+        val btn_signup = findViewById<Button>(R.id.btn_signup_singup)
 
     }
-
 }
